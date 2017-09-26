f4db1453cf0669473097f07394bf9784709bc424
US\sagar.gandewar <Sagar.Gandewar@tavant.com>
2017-09-25 22:32:04 -0700

    Add project files.

---
+++ Exceptions/InvalidFieldException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EncompassREST.Exceptions
+{
+    class InvalidFieldException : Exception
+    {
+        public string Field { get; }
+
+        public InvalidFieldException(string field)
+        {
+            Field = field;
+        }
+    }
+}
---
+++ PipelineModels/Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncompassREST.PipelineModels
+{
+    public class Filter
+    {
+        public Filter()
+        {
+            terms = new List<PipelineModels.IFilter>();
+        }
+
+        public List<IFilter> terms { get; set; }
+
+
+    }
+}
